3a11c6a796fe3432194f6fe6d21470347d1ea6e6
Juan Pablo López Guzmán <juanpablo@findasense.com>
2019-05-19 21:15:06 +0200

    split routes to values for readability

--- src/main/tv/codely/api_scala_http/entry_point/Routes.scala
+++ src/main/tv/codely/api_scala_http/entry_point/Routes.scala
@@ -10,49 +10,57 @@ import scala.concurrent.duration._
 
 final class Routes(container: EntryPointDependencyContainer) {
 
-  val all: Route =
-    get {
-      path("status")(container.statusGetController.get()) ~
-        path("users")(container.userGetController.get()) ~
-        path("videos")(container.videoGetController.get()) ~
-        path("courses")(container.courseGetController.get())
-    } ~
-      post {
-        path("videos") {
-          jsonBody { body =>
-            container.videoPostController.post(
-              body("id").convertTo[String],
-              body("title").convertTo[String],
-              body("duration_in_seconds").convertTo[Int].seconds,
-              body("category").convertTo[String]
-            )
-          }
-        }
-      } ~
-      post {
-        path("users") {
-          jsonBody { body =>
-            container.userPostController.post(
-              body("id").convertTo[String],
-              body("name").convertTo[String],
-            )
-          }
-        }
-      } ~
-      post {
-        path("courses") {
-          jsonBody { body =>
-            container.coursePostController.post(
-              body("id").convertTo[String],
-              body("title").convertTo[String],
-              body("videoId").convertTo[String],
-              body("videoTitle").convertTo[String],
-              body("duration_in_minutes").convertTo[Int].minutes,
-              body("videoCategory").convertTo[String]
-            )
-          }
+  private val status = get {
+    path("status")(container.statusGetController.get())
+  }
+
+  private val user = get {
+    path("users")(container.userGetController.get())
+    } ~ post {
+      path("users") {
+        jsonBody { body =>
+          container.userPostController.post(
+            body("id").convertTo[String],
+            body("name").convertTo[String],
+          )
         }
       }
+    }
+
+  private val video = get {
+    path("videos")(container.videoGetController.get())
+    } ~ post {
+      path("videos") {
+        jsonBody { body =>
+          container.videoPostController.post(
+            body("id").convertTo[String],
+            body("title").convertTo[String],
+            body("duration_in_seconds").convertTo[Int].seconds,
+            body("category").convertTo[String]
+          )
+        }
+      }
+    }
+
+  private val course = get {
+    path("courses")(container.courseGetController.get())
+    } ~ post {
+      path("courses") {
+        jsonBody { body =>
+          container.coursePostController.post(
+            body("id").convertTo[String],
+            body("title").convertTo[String],
+            body("videoId").convertTo[String],
+            body("videoTitle").convertTo[String],
+            body("duration_in_minutes").convertTo[Int].minutes,
+            body("videoCategory").convertTo[String]
+          )
+        }
+      }
+    }
+
+  val all: Route = status ~ user ~ video ~ course
+
   private def jsonBody[T](handler: Map[String, JsValue] => Route): Route =
     entity(as[JsValue])(json => handler(json.asJsObject.fields))
 }
